240d391abf4e3df6e7158482429e9fbc095d9ed5
ajayarepally <arepallyajayladdu@gmail.com>
2025-11-20 00:37:36 +0530

    added link

--- maa_rentals_frontend/src/pages/Register.jsx
+++ maa_rentals_frontend/src/pages/Register.jsx
@@ -1,36 +1,3 @@
-// import React, { useState } from 'react'
-// import api from '../services/api'
-// import { useNavigate } from 'react-router-dom'
-
-// export default function Register() {
-//   const [username, setUsername] = useState('')
-//   const [email, setEmail] = useState('')
-//   const [password, setPassword] = useState('')
-//   const nav = useNavigate()
-
-//   const submit = async (e) => {
-//     e.preventDefault()
-//     try {
-//       await api.post('/auth/register/', { username, email, password })
-//       alert('Registered — please login')
-//       nav('/login')
-//     } catch (err) {
-//       alert('Register failed')
-//     }
-//   }
-
-//   return (
-//     <div className="container mx-auto mt-6 max-w-md">
-//       <h3 className="text-xl mb-3">Register</h3>
-//       <form onSubmit={submit}>
-//         <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} className="form-control mb-2" required />
-//         <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} className="form-control mb-2" required />
-//         <input placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control mb-2" required/>
-//         <button className="btn btn-primary">Register</button>
-//       </form>
-//     </div>
-//   )
-// }
 
 
 import React, { useState } from 'react'
--- maa_rentals_frontend/src/services/api.js
+++ maa_rentals_frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://maa-rentals.onrender.com/api/',
+  baseURL: 'https://maa-rentals-v9vo.onrender.com/api/',
 });
 
 // Add token to each request automatically
