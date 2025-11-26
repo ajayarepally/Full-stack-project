45dc6852f25b69f456ffa97d2e9905558f622873
ajayarepally <arepallyajayladdu@gmail.com>
2025-11-26 15:07:24 +0530

    modified

--- maa_rentals/rent_pro/rent_pro/settings.py
+++ maa_rentals/rent_pro/rent_pro/settings.py
@@ -183,12 +183,11 @@ SIMPLE_JWT = {
 
 # CORS_ALLOW_CREDENTIALS = True 
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://maa-rentals.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
